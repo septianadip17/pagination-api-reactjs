9046ebee6cd9f25356602bf7e5b59abdea486dca
Septian Adi Pratama <septianadip7998@gmail.com>
2023-09-17 21:24:47 +0700

    penambahan metode axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ReactPaginate from "react-paginate";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -11,35 +12,68 @@ function App() {
 
   useEffect(() => {
     const getComments = async () => {
-      const res = await fetch(
-        `http://localhost:3004/comments?_page=1&_limit=12`
-      );
-      // const res = await fetch(
-      //   `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=12`
-      // );
-      const data = await res.json();
-      const total = res.headers.get('x-total-count');
-      setPageCount(Math.ceil(total/limit));
-      console.log(Math.ceil(total/limit));
-      console.log(total)
-      setItems(data);
+      try {
+        const response = await axios.get(
+          `http://localhost:3004/comments?_page=1&_limit=12`
+        );
+        const data = response.data;
+        const total = response.headers["x-total-count"];
+        setPageCount(Math.ceil(total / limit));
+        setItems(data);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     getComments();
-  }, []);
+    
+  }, [limit, setPageCount])
+
+  // useEffect(() => {
+  //   const getComments = async () => {
+  //     const res = await fetch(
+  //       `http://localhost:3004/comments?_page=1&_limit=12`
+  //     );
+  //     // const res = await fetch(
+  //     //   `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=12`
+  //     // );
+  //     const data = await res.json();
+  //     const total = res.headers.get('x-total-count');
+  //     setPageCount(Math.ceil(total/limit));
+  //     console.log(Math.ceil(total/limit));
+  //     console.log(total)
+  //     setItems(data);
+  //   };
+
+  //   getComments();
+  // }, []);
 
   console.log(items);
-
+// cara menjalankannya: json-server --watch db.json --port 3004
   const fetchComments = async (currentPage) => {
-    const res = await fetch(
-      `http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
-    );
-    // const res = await fetch(
-    //   `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
-    // );
-    const data = await res.json();
-    return data;
-  }
+    try {
+      const response = await axios.get(
+        `http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
+      );
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+  
+
+  // const fetchComments = async (currentPage) => {
+  //   const res = await fetch(
+  //     `http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
+  //   );
+  //   // const res = await fetch(
+  //   //   `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
+  //   // );
+  //   const data = await res.json();
+  //   return data;
+  // }
 
   const handlePageClick = async (data) => {
     console.log(data.selected);
